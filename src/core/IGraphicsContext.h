df0f47e543845ccc69d4cde2c265365364040d7c
Michal <dvorkam@gmail.com>
2025-12-20 12:22:55 +0100

    feat: Enhance IGraphicsContext with fill_rect and measure_text methods

--- src/core/IGraphicsContext.h
+++ src/core/IGraphicsContext.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <string>
+
+// Forward declare Rect to break dependency cycle
+namespace Hummingbird::Layout { struct Rect; }
+
 struct Color {
     unsigned char r, g, b, a;
+};
+
+struct TextMetrics {
+    float width;
+    float height;
 };
 
 class IGraphicsContext {
@@ -10,4 +20,7 @@ public:
 
     virtual void clear(const Color& color) = 0;
     virtual void present() = 0;
+    virtual void fill_rect(const Hummingbird::Layout::Rect& rect, const Color& color) = 0;
+    virtual TextMetrics measure_text(const std::string& text, const std::string& font_path, float font_size) = 0;
+    virtual void draw_text(const std::string& text, float x, float y, const std::string& font_path, float font_size) = 0;
 };
