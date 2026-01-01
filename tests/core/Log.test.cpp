e48d36b82da6929b32066f6edabf34d5583c9687
dvorkam <dvorkam@example.com>
2026-01-01 10:36:28 +0100

    Switch logging to a single log level

--- tests/core/Log.test.cpp
+++ tests/core/Log.test.cpp
@@ -14,9 +14,15 @@ TEST(LogTest, EmitsWhenEnabled) {
     std::cerr.rdbuf(old);
     auto output = buffer.str();
 
-#ifdef HB_ENABLE_LOG
+#if HB_LOG_LEVEL >= 3
     EXPECT_NE(output.find("[info] hello"), std::string::npos);
     EXPECT_NE(output.find("[warn] world"), std::string::npos);
+#elif HB_LOG_LEVEL >= 2
+    EXPECT_EQ(output.find("[info] hello"), std::string::npos);
+    EXPECT_NE(output.find("[warn] world"), std::string::npos);
+#elif HB_LOG_LEVEL >= 1
+    EXPECT_EQ(output.find("[info] hello"), std::string::npos);
+    EXPECT_EQ(output.find("[warn] world"), std::string::npos);
 #else
     EXPECT_TRUE(output.empty());
 #endif
